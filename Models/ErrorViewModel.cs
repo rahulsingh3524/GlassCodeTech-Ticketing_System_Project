6b9695f85e8f9c213da420b6373b49a03ca73102
Rahul Singh <rahuldhakad850@gmail.com>
2025-08-18 21:28:50 +0530

    Initial push.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlassCodeTech_Ticketing_System_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
